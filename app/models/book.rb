dc54de2e64804e1251cb05d1b0929e1ec477f835
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-01-29 13:37:04 +0000

    [add]コメント機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
+
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
