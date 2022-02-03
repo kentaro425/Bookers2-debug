322ba1725c6d9632278544366a4fcf3f10ad58fd
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-02-03 13:06:13 +0000

    [add]コメント非同期通信

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,15 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
